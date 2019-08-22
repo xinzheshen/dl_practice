a7093beece25d9647e1d0c364e5f4d47ad2ee628
xinzheshen <xinzhe.shen@cienet.com.cn>
2019-08-22 20:28:57 +0800

    add parts of course4_03

--- src/test.py
+++ src/test.py
@@ -32,3 +32,36 @@ print(str(d[:,1]))
 a = np.array([[1,2],[3,4]])
 b = np.array([[11,12],[13,14]])
 print(np.dot(a,b))
+
+
+print("最大索引-------------")
+a = np.array([3, 1, 2, 4, 6, 1])
+print(np.argmax(a))
+print("-----")
+a = np.array([[1, 5, 5, 2],
+              [9, 6, 2, 8],
+              [3, 7, 9, 1]])
+# 沿着axis=0， 相当于在各个行之间比较相同列的大小，结果[1 2 2 1]
+print(np.argmax(a, axis=0))
+print("--")
+# 沿着axis=1， 相当于在各个列之间比较相同行的大小，结果[1 0 2]
+print(np.argmax(a, axis=1))
+
+print("-----三维")
+a = np.array([
+    [
+        [1, 5, 5, 2],
+        [9, -6, 2, 8],
+        [-3, 7, -9, 1]
+    ],
+
+    [
+        [-1, 5, -5, 2],
+        [9, 6, 2, 8],
+        [3, 7, 9, 1]
+    ]
+])
+print("shape", a.shape)
+print(np.argmax(a, axis=0))
+print("--")
+print(np.argmax(a, axis=1))
